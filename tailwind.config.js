5ba0378142757120af8c1d7750a5fa1ed3d1d4e5
Ruhul1994 <k.m.ruhulkuddus@gmail.com>
2024-07-04 19:22:42 +0600

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode:'class',
+  content: [
+    './pages/**/*.{html,js}',
+    './components/**/*.{html,js}',
+    './index.html'
+  ],
+  // ...
+}
